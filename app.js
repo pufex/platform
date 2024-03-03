7d5a0a6e7fc3b3274e23d7653bb9006a41a269de
Jakub Abram <78880541+pufex@users.noreply.github.com>
2024-03-03 17:28:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,33 +9,44 @@ menu.addEventListener("submit", (e) => {
 
 class Player{
     constructor(x,y, name){
-        this.h = 80;
-        this.w = 25;
+        this.height = 80;
+        this.width = 25;
         this.name = name;
-        this.sprites = {
-            static: "/static.svg",
-            goes: "/move.svg",
-        }
-        this.spawnCords = [x,y]
+        this.spawnCords = [x,y];
         this.setCords = function(w,h,spawnCords){
-            let newCords = {tl, tr, bl, br};
+            let newCords = {tl: "", tr: "", bl: "", br: ""};
             newCords.tl = [spawnCords[0], spawnCords[1]];
             newCords.tr = [spawnCords[0] + w, spawnCords[1]];
             newCords.bl = [spawnCords[0], spawnCords[1]+h];
             newCords.br = [spawnCords[0] + w, spawnCords[1] + h];
             return newCords;
         }
-        this.hitbox = setCords(this.w, this.h, this.spawnCords)
+        this.setSprites = function(images = []){
+            let array = [];
+            images.forEach((element) => {
+                let img = new Image();
+                img.src = element;
+                array.push(img);
+            })
+            return array;
+        };
+        this.sprites = this.setSprites(["/static.svg","/move.svg"]);
+        this.hitbox = this.setCords(this.w, this.h, this.spawnCords);
     };
-    drawPlayer(pctx, x,y, sprite){
-        pctx.save();
-        pctx.translate(x,y);
-        pctx.drawImage(sprite, 0,0, this.width, this.height, 0, 0, this.width, this.height);
-        pctx.restore();
+    drawPlayer(ctx, x,y, w, h){
+        ctx.save();
+        ctx.translate(x,y);
+        ctx.fillStyle = "red";
+
+        ctx.fillRect(0,0, w, h);
+        // pctx.drawImage(sprite, 0,0, this.width, this.height, 0, 0, this.width, this.height);
+        ctx.restore();
     }
 }
 
 let player = new Player(50,50, username);
+
+console.log(player)
 
 const canvasOne = document.querySelector("#player")
 canvasOne.ctx = canvasOne.getContext("2d");
@@ -45,6 +56,32 @@ canvasOne.height = window.innerHeight;
 
 let animId, vx = 2, vy = 0, v;
 
-animId = requestAnimationFrame(() => {  
+
+const body = document.querySelector("body");
+
+// canvasOne.ctx.fillStyle = "black"; 
+// canvasOne.ctx.fillRect(0,0, canvasOne.width, canvasOne.height);
+
+const canFall = (ctx, vx, w, cord) => {
+
+    for(let i = 0; i < w; i++){
+        if(false){
+            if(vx == 1) return 0;
+            else return canFall(Math.ceil(vx/2), w, h, cord);
+        }else{
+            return vx;
+        }
+    }
+}
+
+let plainPixels;
+console.log(player.hitbox)
+
+
+animId = setInterval(() => {  
+    let plainPixels = canvasOne.ctx.getImageData() 
+    canvasOne.ctx.clearRect(0,0, window.innerWidth, window.innerHeight);
+    player.drawPlayer(canvasOne.ctx, 600, 600, player.width, player.height);
 
 })
+
